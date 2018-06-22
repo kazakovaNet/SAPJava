6ae6a57b3a52df88e09bdea55c2103452f36f749
nkazakova <natalia.kazakova@innoway.ru>
2018-06-23 00:25:17 +0300

    First commit

---
+++ src/for_loops_exercise_1/Gadget.java
@@ -0,0 +1,10 @@
+package for_loops_exercise_1;
+
+class Gadget {
+
+    void countTo(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/for_loops_exercise_1/Story.java
@@ -0,0 +1,14 @@
+package for_loops_exercise_1;
+
+import for_loops_exercise_1.Gadget;
+
+class Story {
+
+    public static void main(String args[]) {
+        Gadget gadget = new Gadget();
+        // the gadget shall count to 24
+        // change the number here, if you want to try out other upper limits.
+        gadget.countTo(24);
+    }
+
+}
